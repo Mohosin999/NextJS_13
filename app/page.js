83f4f69e85f61fc36df079137d872a7a4c9919cd
Mohosin999 <programmermohosin@gmail.com>
2023-06-11 06:40:40 +0600

    page file changed

--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
-"use client";
 import React from "react";
-import styled from "styled-components";
-
-const MyName = styled.h2`
-  font-size: 2rem;
-  color: red;
-  &:hover {
-    color: green;
-  }
-`;
 
 const page = () => {
   return (
     <div>
-      <MyName>Mohosin Hasan</MyName>
+      <h2>Mohosin Hasan</h2>
     </div>
   );
 };
