e4e18db3503165040bec210156fd58b5153abf30
Mohosin999 <programmermohosin@gmail.com>
2023-06-11 06:59:06 +0600

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const page = () => {
+const HomePage = () => {
   return (
     <div>
-      <h2>Mohosin Hasan</h2>
+      <h1>This is our Home Page</h1>
     </div>
   );
 };
 
-export default page;
+export default HomePage;
